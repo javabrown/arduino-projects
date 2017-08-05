b5e32cfce036c21dbf59cffdd70673e365062235
Raja Khan <getrk@yahoo.com>
2017-08-04 23:41:00 -0400

    Update stepper.c

--- stepper/stepper.c
+++ stepper/stepper.c
@@ -1,25 +1,28 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 100;  //this is to fit the number of steps per revolutio for your motor
- 
+#define STEPS_PER_MOTOR_REVOLUTION 32
+#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
+
 int In1Port = 8;
 int In2Port = 9;
 int In3Port = 10;
 int In4Port = 11;
 
-Stepper myStepper(stepsPerRevolution, In1Port, In3Port, In2Port, In4Port);
+Stepper stepper(STEPS_PER_MOTOR_REVOLUTION, In1Port, In3Port, In2Port, In4Port);
+ 
+int  steps2Take;
 
-void setup() {
-  myStepper.setSpeed(60);
-  Serial.begin(9600);
+void setup(){ 
 }
 
 void loop() {
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
-
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
+  steps2Take  =  STEPS_PER_OUTPUT_REVOLUTION ;  // Rotate CW 1 turn
+  stepper.setSpeed(500);
+  stepper.step(steps2Take);
+  delay(1000);
+  
+  steps2Take  =  - STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CCW 1 turn  
+  stepper.setSpeed(500);  // 700 a good max speed??
+  stepper.step(steps2Take);
+  delay(2000);
 }
